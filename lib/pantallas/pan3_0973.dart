39422acbb113ce478e1d7e880cddfc75befa05d8
NAIROBI_KEREN_MONTIEL_SERRANO <a.21308051280973@cbtis128.edu.mx>
2024-03-27 22:42:10 +0000

    cambio p3

--- lib/pantallas/pan3_0973.dart
+++ lib/pantallas/pan3_0973.dart
@@ -13,40 +13,46 @@ class Pantalla3_0973 extends StatelessWidget {
           backgroundColor: const Color(0xffef5350),
           title: const Text('Pantalla 3 Montiel0973'),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(40),
-              width: 300,
-              height: 90,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                color: const Color(0xFFef5350), //blue
-                borderRadius: BorderRadius.circular(45),
-              ),
-              child: Container(
-                width: 210,
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(40),
+                width: 300,
                 height: 90,
-                decoration: const BoxDecoration(
-                  color: Color(0xFFffcdd2), //light blue
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(45),
-                    bottomLeft: Radius.circular(45),
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFef5350), //blue
+                  borderRadius: BorderRadius.circular(45),
+                ),
+                child: Container(
+                  width: 210,
+                  height: 90,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFffcdd2), //light blue
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(45),
+                      bottomLeft: Radius.circular(45),
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'BoleNais',
+                    style: TextStyle(fontSize: 32, color: Colors.black),
                   ),
                 ),
-                alignment: Alignment.center,
-                child: const Text(
-                  'BoleNais',
-                  style: TextStyle(fontSize: 32, color: Colors.black),
-                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              child: const Text('Mat. 21308051280973',
-                  style: TextStyle(fontSize: 30)),
-            )
-          ],
+              Container(
+                  padding: const EdgeInsets.all(20),
+                  child: const Column(
+                    children: [
+                      Text('Nairobi Montiel', style: TextStyle(fontSize: 25)),
+                      Text('Mat. 21308051280973',
+                          style: TextStyle(fontSize: 25))
+                    ],
+                  ))
+            ],
+          ),
         ));
   }
 }
